abd4f47101729968eafd06999ade9ea9e8ded3da
hxxwon2 <jo9770@naver.com>
2023-11-18 18:24:44 +0900

    week12

--- routes/posts.js
+++ routes/posts.js
@@ -9,4 +9,9 @@ router.get('/insert', function(req, res) {
     res.render('index', { title: '글쓰기',pageName:'posts/insert.ejs' });
   });
 
+router.get('/read', function(req, res, next) {
+  const id=req.query.id;
+  res.render('index', { title: '게시글정보',pageName:'posts/read.ejs', id });
+});  
+
 module.exports = router;
